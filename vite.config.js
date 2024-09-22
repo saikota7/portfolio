f727bf5821932a2a88c9c8b965f9ac624d6b556c
Sai Kota <96785985+saikota7@users.noreply.github.com>
2024-09-22 13:35:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/',  // Add this line to fix your GitHub Pages deployment
   plugins: [react()],
 })
